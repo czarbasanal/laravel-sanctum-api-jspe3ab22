ffb66741071a1390d2f0aae308c40d1b0d0853e4
czarbasanal <czarbasanal@gmail.com>
2024-07-12 22:55:06 +0800

    implemented successfully

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,18 @@
 
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+// Authentication routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('posts', PostController::class);
     Route::post('comments', [CommentController::class, 'store']);
     Route::put('comments/{comment}', [CommentController::class, 'update']);
-    Route::delete('comments/{comment}', [
-        CommentController::class,
-        'destroy'
-    ]);
+    Route::delete('comments/{comment}', [CommentController::class, 'destroy']);
 });
